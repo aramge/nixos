f52fa66aa518ae5e404f2760f85ac61ebe8af418
ramge@n100-nixos <axel@ramge.de>
2026-03-22 21:36:47 +0100

    nfs3

--- common/nfs-turing.nix
+++ common/nfs-turing.nix
@@ -4,13 +4,13 @@
   fileSystems."/mnt/turing/files" = {
     device = "turing:/zroot/data/files";
     fsType = "nfs";
-    options = [ "x-systemd.automount" "noauto" "x-systemd.idle-timeout=600" "nolock" "soft" ];
+    options = [ "nfsvers=3" "x-systemd.automount" "noauto" "x-systemd.idle-timeout=600" "nolock" "soft" ];
   };
 
   fileSystems."/mnt/turing/gh" = {
     device = "turing:/home/ramge/sync/gh";
     fsType = "nfs";
-    options = [ "x-systemd.automount" "noauto" "x-systemd.idle-timeout=600" "nolock" "soft" ];
+    options = [ "nfsvers=3" "x-systemd.automount" "noauto" "x-systemd.idle-timeout=600" "nolock" "soft" ];
   };
   systemd.tmpfiles.rules = [
     "d /home/ramge/sync 0755 ramge ramge -"
