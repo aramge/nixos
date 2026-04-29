61bca80960f7176105544c9133589a0a845b339c
ramge@mbp3 <axel@ramge.de>
2026-04-29 05:55:44 +0200

    Zwischenstand

--- hosts/mnixos/configuration.nix
+++ hosts/mnixos/configuration.nix
@@ -5,73 +5,75 @@
     ./hardware-configuration.nix
     ../../common/default.nix
     ../../common/desktop.nix
-    ../../common/niri.nix
   ];
 
+  # --- Boot & Kernel ---
   boot.loader = {
     systemd-boot.enable = true;
     efi.canTouchEfiVariables = true;
   };
-
   boot.kernelParams = [ "video=Virtual-1:2560x1440@60" ];
 
+  # --- Hardware & Netzwerk ---
+  hardware.enableRedistributableFirmware = true;
+  
   networking = {
     hostName = "mnixos";
     networkmanager.enable = true;
   };
 
-  hardware.enableRedistributableFirmware = true;
+  # --- System-Dienste & VM-Integration ---
   security.rtkit.enable = true;
 
   services = {
+    qemuGuest.enable = true;
+    spice-vdagentd.enable = true;
+
     libinput = {
       enable = true;
       mouse.naturalScrolling = true;
       touchpad.naturalScrolling = true;
     };
+
+    # Audio-Stack
+    pipewire = { 
+      enable = true; 
+      alsa.enable = true; 
+      pulse.enable = true; 
+    };
+
+    # Mac-Tastatur-Korrektur für die VM
     xserver.xkb = lib.mkForce { layout = "de"; variant = "mac_nodeadkeys"; };
     udev.extraHwdb = ''
       evdev:input:b*v*p*e*
-       KEYBOARD_KEY_70035=102nd
-       KEYBOARD_KEY_70064=grave
+        KEYBOARD_KEY_70035=102nd
+        KEYBOARD_KEY_70064=grave
     '';
-    spice-vdagentd.enable = true;
-    qemuGuest.enable = true;
-    pipewire = { enable = true; alsa.enable = true; pulse.enable = true; };
   };
 
-  # UTM Shared Folder
-  # 1. Der rohe, versteckte Mount-Punkt
+  # --- UTM Shared Folder (9p -> Bindfs) ---
   fileSystems."/mnt/mac-share-raw" = {
     device = "share";
     fsType = "9p";
     options = [ "trans=virtio" "version=9p2000.L" "msize=1048576" "nofail" ];
   };
 
-  # 2. Die Übersetzer-Schicht für deinen User
   fileSystems."/mnt/mac-share" = {
     device = "/mnt/mac-share-raw";
     fsType = "fuse.bindfs";
     depends = [ "/mnt/mac-share-raw" ];
     options = [ 
-      "force-user=ramge"        # In der VM gehört alles dir
-      "force-group=users"
-      "create-for-user=501"     # Neue Dateien bekommen auf dem Mac die 501
-      "create-for-group=20"     # 20 ist die Standard 'staff'-Gruppe auf macOS
-      "allow_other"             # Erlaubt dem Root-User (für Rebuilds) den Zugriff
+      "force-user=ramge" 
+      "force-group=users" 
+      "create-for-user=501" 
+      "allow_other" 
       "nofail" 
     ];
   }; 
   systemd.tmpfiles.rules = [ "L+ /home/ramge/sync - - - - /mnt/mac-share" ];
-  
-  environment.systemPackages = with pkgs; [ brightnessctl spice-vdagent ];
 
-  home-manager = {
-    useGlobalPkgs = true;
-    useUserPackages = true;
-    backupFileExtension = "bak";
-    users.ramge = import ../../users/ramge/home.nix;
-  };
+  # --- Pakete & User ---
+  environment.systemPackages = with pkgs; [ brightnessctl spice-vdagent ];
 
   system.stateVersion = "25.11";
 }
