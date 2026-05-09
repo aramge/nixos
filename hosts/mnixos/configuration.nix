4d6e61eadb02b39733edf6cad65aeab18ff40a98
ramge@mbp3 <axel@ramge.de>
2026-05-09 07:16:54 +0200

    Anpassung mnixos

--- hosts/mnixos/configuration.nix
+++ hosts/mnixos/configuration.nix
@@ -1,10 +1,10 @@
 { config, pkgs, lib, ... }:
-
 {
   imports = [
     ./hardware-configuration.nix
     ../../common/default.nix
     ../../common/desktop.nix
+    ../../common/gnome-paperwm.nix
   ];
 
   # --- Boot & Kernel ---
@@ -16,7 +16,7 @@
 
   # --- Hardware & Netzwerk ---
   hardware.enableRedistributableFirmware = true;
-  
+  hardware.pulseaudio.enable = false;
   networking = {
     hostName = "mnixos";
     networkmanager.enable = true;
@@ -24,25 +24,21 @@
 
   # --- System-Dienste & VM-Integration ---
   security.rtkit.enable = true;
-
+  virtualisation.docker.enable = true;
   services = {
     qemuGuest.enable = true;
     spice-vdagentd.enable = true;
-
     libinput = {
       enable = true;
       mouse.naturalScrolling = true;
       touchpad.naturalScrolling = true;
     };
-
-    # Audio-Stack
-    pipewire = { 
-      enable = true; 
-      alsa.enable = true; 
-      pulse.enable = true; 
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
     };
-
-    # Mac-Tastatur-Korrektur für die VM
     xserver.xkb = lib.mkForce { layout = "de"; variant = "mac_nodeadkeys"; };
     udev.extraHwdb = ''
       evdev:input:b*v*p*e*
@@ -57,23 +53,27 @@
     fsType = "9p";
     options = [ "trans=virtio" "version=9p2000.L" "msize=1048576" "nofail" ];
   };
-
   fileSystems."/mnt/mac-share" = {
     device = "/mnt/mac-share-raw";
     fsType = "fuse.bindfs";
     depends = [ "/mnt/mac-share-raw" ];
-    options = [ 
-      "force-user=ramge" 
-      "force-group=users" 
-      "create-for-user=501" 
-      "allow_other" 
-      "nofail" 
+    options = [
+      "force-user=ramge"
+      "force-group=users"
+      "create-for-user=501"
+      "allow_other"
+      "nofail"
     ];
-  }; 
+  };
   systemd.tmpfiles.rules = [ "L+ /home/ramge/sync - - - - /mnt/mac-share" ];
 
   # --- Pakete & User ---
   environment.systemPackages = with pkgs; [ brightnessctl spice-vdagent ];
+  users.users.ramge.initialPassword = "ramge";
+
+  home-manager.useGlobalPkgs = true;
+  home-manager.useUserPackages = true;
+  home-manager.backupFileExtension = "bak";
 
   system.stateVersion = "25.11";
 }
